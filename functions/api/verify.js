6d595beff6432045dfb091afacad7b6861017c21
123nihao321-spec <123nihao321@gmail.com>
2026-01-04 21:45:13 +0800

    Update verify.js

--- functions/api/verify.js
+++ functions/api/verify.js
@@ -2,9 +2,8 @@ export async function onRequestPost(context) {
   try {
     const { password } = await context.request.json();
     
-    // 获取 Cloudflare 环境变量，如果没有设置，自动回退到 "888"
-    // 这样保证了您在本地运行或环境变量未生效时也能登录
-    const correctPassword = context.env.ADMIN_PASSWORD || "888";
+    // 获取 Cloudflare 环境变量
+    const correctPassword = context.env.ADMIN_PASSWORD;
 
     if (password === correctPassword) {
       return new Response(JSON.stringify({ success: true }), { status: 200 });
@@ -14,4 +13,5 @@ export async function onRequestPost(context) {
   } catch (e) {
     return new Response(JSON.stringify({ success: false, message: e.message }), { status: 500 });
   }
+
 }
